20aade06f727c1838f64c0513282cd5d96e22f68
myayric <7068082@gmail.com>
2022-09-07 22:40:36 +0300

    first

--- 3_1.py
+++ 3_1.py
@@ -1,10 +1,8 @@
-#Пользователь вводит предложение, заменить все пробелы на "-" 2-мя способами
+# Пользователь вводит предложение, заменить все пробелы на "-" 2-мя способами
 
 
 text = input("Type the text: ")
 spl1 = text.split()
-print ("The first method is :", "_" .join(spl1))
-
-
+print("The first method is :", "_".join(spl1))
 text2 = text.replace(" ", "_")
 print("The second method is :", text2)
