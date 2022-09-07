8ec78c1c682837864c869223ed2e5c49a4b6f3d5
70680 <7068082@gmail.com>
2022-09-07 16:46:05 +0300

    Первое домашнее задание - часть первая

---
+++ 3_1.py
@@ -0,0 +1,10 @@
+#Пользователь вводит предложение, заменить все пробелы на "-" 2-мя способами
+
+
+text = input("Type the text: ")
+spl1 = text.split()
+print ("The first method is :", "_" .join(spl1))
+
+
+text2 = text.replace(" ", "_")
+print("The second method is :", text2)
