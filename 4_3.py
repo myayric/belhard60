3bbc39f5712e0191a85d257153861df3893ca920
70680 <7068082@gmail.com>
2022-09-08 09:51:49 +0300

    4

--- 4_3.py
+++ 4_3.py
@@ -4,7 +4,10 @@
 first_num = input("Please enter the first number: ")
 second_num = input("Please enter the second number ")
 third_num = input("Please enter the third number ")
-zero_num = first_num.count('0') + second_num.count('0') + third_num.count('0')
+first_num_int = float(first_num)
+second_num_int = float(second_num)
+third_num_int = float(third_num)
+zero_num = int(first_num_int==0) + int(second_num_int==0) + int(third_num_int==0)
 neg_num = first_num.count('-') + second_num.count('-') + third_num.count('-')
 pos_num = 3 - zero_num - neg_num
 print("Number of negative numbers is: ", neg_num)
