fc16bfdf6528b781abfc2f16ea2cb47a85e364c0
myayric <7068082@gmail.com>
2022-09-14 23:35:37 +0300

    th2

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-n = int(input())
-num = [i*2 for i in range(1, n+1)]
-print(sum(num))
+# m = int(input())
+# mom = [i for i in range(1, m + 1)]
+# print(sum(mom))
+# # print(mom)
+# # del mom[2]
+# # print(mom)
+# # mom.remove(4)
+# # print(mom)
+# # n = int(input("2: "))
+# # n2 = [i for i in range(1, n + 1)]
+# # print(n2)
+# # nona = mom + n2
+# # # print(nona)
+# # # print(len(nona))
+# # # print(len(mom))
+# # # print(len(n2))
+# # print(n2[1:2:3])
+# # print(max(nona))
+# # mom.append(89)
+# # print(mom)
+# # mom.insert(5,"new")
+# # print(mom)
+
