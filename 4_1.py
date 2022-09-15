419b397d1342103eaaf80541b9a75b779b9dbc35
ArtyomVabishevich <avabishevich@mail.ru>
2022-09-15 21:31:46 +0300

    Revert "add pattern to this"
    This reverts commit 4ef5b6cd

---
+++ 4_1.py
@@ -0,0 +1,5 @@
+# Заполнить список степенями числа 2 (от 2^1 до 2^n).
+number = int(input("Enter the number: "))
+num = [2**i for i in range(1, number + 1)]
+print(num)
+print("sum: ", sum(num))
---
+++ class.py
@@ -0,0 +1,37 @@
+# text = input()
+# for i in text:
+#     if text.count(i) == 1:
+#         print(i)
+
+# text = input()
+# while not text.isdigit():
+#    text = input("enter ")
+
+#
+# text = input("enter: ")
+# for i in text:
+#     if text(i) == text(i+1):
+#         print(i)
+
+
+# x = float(input("X: "))
+# y = float(input("Y: "))
+# d = 1
+# while x < y:
+#      x *= 1.1
+#      d += 1
+# print(d)
+
+from random import randint
+x = randint(1, 10)
+print(x)
+c = 0
+while x != y:
+    if y < x:
+        print ("Y<X")
+
+    elif y > x:
+        print("Y>x")
+
+        print(c)
+print(x)
