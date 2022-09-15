1221c6e631371d62de8a787077c9ccebd1e7885c
70680 <7068082@gmail.com>
2022-09-15 16:30:58 +0300

    Заполнить словарь где ключами будут выступать числа от 0 до n, а значениями вложенный словарь с ключами "name" и "email", а значения для этих ключей будут браться с клавиатуры

---
+++ 4_3.py
@@ -0,0 +1,10 @@
+# Заполнить словарь где ключами будут выступать числа от 0 до n, а
+# значениями вложенный словарь с ключами "name" и "email", а значения
+# для этих ключей будут браться с клавиатуры..
+
+
+num = int(input("Enter the desired number: "))
+text = {i: {"name": input("\n\nEnter your name: "),
+            "email": input("Enter your Email: ")}
+        for i in range(0, num)}
+print("your file for name and Email is: \n", text)
