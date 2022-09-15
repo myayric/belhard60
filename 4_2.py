0016c1fdac4c8cf948ed4b86b70b28965b49b464
70680 <7068082@gmail.com>
2022-09-15 16:06:09 +0300

    Без использования collections, написать программу, которая будет создавать словарь для подсчитывания количества вхождений каждой # буквы в текст введенный с клавиатуры

--- 4_2.py
+++ 4_2.py
@@ -5,6 +5,6 @@
 
 slov = input("Enter your sentence:  ")
 slov2 = slov.replace(" ", "")
-Result = [slov2.lower()[i] for i in range(0, len(slov2))]
+Result = [slov2.upper()[i] for i in range(0, len(slov2))]
 num_slov = {Result[i]: Result.count((Result[i])) for i in range(0, len(slov2))}
 print(num_slov)
