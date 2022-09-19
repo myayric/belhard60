4b76bd97ec7eeaa49fca7bb2cb75c8a7729a25f6
70680 <7068082@gmail.com>
2022-09-19 11:44:57 +0300

    Сделать калькулятор: у пользователя спрашивается число, потом действие и второе число

--- 4_2.py
+++ 4_2.py
@@ -2,9 +2,10 @@
 # создавать словарь для подсчитывания количества вхождений каждой
 # буквы в текст введенный с клавиатуры
 
-
+import collections
 slov = input("Enter your sentence:  ")
 slov2 = slov.replace(" ", "")
-Result = [slov2.upper()[i] for i in range(0, len(slov2))]
-num_slov = {Result[i]: Result.count((Result[i])) for i in range(0, len(slov2))}
+#Result = [slov2.upper()[i] for i in range(0, len(slov2))]
+#num_slov = {Result[i]: Result.count((Result[i])) for i in range(0, len(slov2))}
+slov2 = _collections.counter
 print(num_slov)
