151581f563310c9a28cc6a891d24e37b2ad799c7
70680 <7068082@gmail.com>
2022-09-19 11:45:17 +0300

    Сделать калькулятор: у пользователя спрашивается число, потом действие и второе число

--- 4_2.py
+++ 4_2.py
@@ -5,7 +5,7 @@
 import collections
 slov = input("Enter your sentence:  ")
 slov2 = slov.replace(" ", "")
-#Result = [slov2.upper()[i] for i in range(0, len(slov2))]
-#num_slov = {Result[i]: Result.count((Result[i])) for i in range(0, len(slov2))}
+Result = [slov2.upper()[i] for i in range(0, len(slov2))]
+num_slov = {Result[i]: Result.count((Result[i])) for i in range(0, len(slov2))}
 slov2 = _collections.counter
 print(num_slov)
