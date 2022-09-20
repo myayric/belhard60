dde080fbb33d78acf49789bfeecc6509ea1c006e
70680 <7068082@gmail.com>
2022-09-20 12:46:13 +0300

    s

--- 5_3.py
+++ 5_3.py
@@ -1,11 +1,11 @@
-#Вывести четные числа от 2 до N по 5 в строку
+# Вывести четные числа от 2 до N по 5 в строку
 
-number_n = int(input("введите число: "))
-c = 0
-for i in range(2, number_n+1):
-    if not i % 2 and c < 5:
+num = int(input("Enter the number: "))
+ent = 0
+for i in range(2, num + 1):
+    if not i % 2 and ent < 5:
         print(i, end=" ")
-        c += 1
-    elif c == 5:
+        ent += 1
+    elif ent == 5:
         print()
-        c = 0
+        ent = 0
