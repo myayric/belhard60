e1f90b613b99f2b16bf5f705e3faf0389498225d
70680 <7068082@gmail.com>
2022-09-27 13:29:03 +0300

    Дан список чисел и на вход поступает число N

--- 6_3.py
+++ 6_3.py
@@ -2,6 +2,11 @@
 # указанное число, пример: [1,2,3,4,5,6,7] N=3 ответ: [5,6,7,1,2,3,4]
 
 
-my_list = [1, 2, 3, 4, 5, 6, 7]
-n = int(input("Choose your number between 1 and 7\n >>> "))
-print(my_list[len(my_list) - n:] + my_list[0:len(my_list) - n])
+lst = []
+num_el = int(input("Enter number of elements : "))
+for i in range(0, num_el):
+    ele = int(input())
+    lst.append(ele)
+print("Your list of numbers is \n>>>", lst)
+n = int(input(f"Choose your number between 1 to {num_el}\n>>> "))
+print(lst[len(lst) - n:] + lst[0:len(lst) - n])
